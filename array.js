2fc5f37d27cbfabdb504a28dd18ed103919799dc
Ramankumarlal <ramankumarlal@outlook.com>
2023-04-01 13:43:17 +0530

    file added

---
+++ array.js
@@ -0,0 +1,27 @@
+// const myArr = [1, 2, 3, 4, 5];
+
+// // console.log(myArr[2]);
+
+// myArr.push(6, 7);
+// myArr.push(8, 9, 10);
+// // console.log(myArr);
+// // myArr.pop(); // in pop any argument is not required it simply pop out's the last value from th array.
+// // console.log(myArr);
+// myArr.unshift("Rishabh"); //it adds a value to the start of the array
+// console.log(myArr);
+// myArr.shift(); //noargument is required. it removes the first element fron the array.
+// console.log(myArr);
+
+// console.log(myArr.includes(12));
+
+// console.log(myArr.indexOf(10));
+
+const myArr = [1, 2, 3, 4, 5];
+// const newArr = myArr.join();
+ 
+ console.log(myArr(2)); // object
+
+// console.log(newArr);
+// console.log(typeof newArr); //string
+
+// slice and splice
---
+++ node.js
@@ -0,0 +1,26 @@
+// const courses = [
+//     {
+//         name: "Complete Reactjs course",
+//         price: "2.3",
+//     },
+//     {
+//         name: "Complete Angulur course",
+//         price: "2.7",
+//     },  
+//     {
+//         name: "Complete MERN course",
+//         price: "2.9",
+//     },
+//     {
+//         name: "Complete C++ course",
+//         price: "2.95",
+//     },
+    
+// ];
+// how to loop through an array?
+let a = 2;
+let b = 3;
+
+console.log(a + b);
+
+
